384883655fd69cf0104ef74e748ae8ab7e7de40f
Eduardo Galera <galera.eduardo@gmail.com>
2024-11-07 18:26:24 -0300

    gateway

--- setup.tf
+++ setup.tf
@@ -5,6 +5,14 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "MyInternetGateway"
+  }
+}
+
 resource "aws_subnet" "my_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
@@ -14,6 +22,66 @@ resource "aws_subnet" "my_subnet" {
   tags = {
     Name = "MySubnet"
   }
+}
+
+resource "aws_subnet" "my_private_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "MyPrivateSubnet"
+  }
+}
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "my_nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.my_subnet.id
+
+  tags = {
+    Name = "MyNatGateway"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "MyPublicRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "public_rt_assoc" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.my_nat_gw.id
+  }
+
+  tags = {
+    Name = "MyPrivateRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_assoc" {
+  subnet_id      = aws_subnet.my_private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
 }
 
 resource "aws_security_group" "my_sg" {
@@ -47,7 +115,6 @@ resource "aws_security_group" "my_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Añadir regla para permitir acceso SSH en el puerto 22
   ingress {
     from_port   = 22
     to_port     = 22
