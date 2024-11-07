a2236ad9e05e11faca2c5d74148d7d9df395b816
Eduardo Galera <82102759+ecgalera@users.noreply.github.com>
2024-11-07 17:05:17 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,25 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "my_instance" {
-  ami           = "ami-0984f4b9e98be44bf" # Amazon Linux 2 AMI
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
   key_name      = var.key_name
 
   vpc_security_group_ids = [aws_security_group.my_sg.id]
+  subnet_id              = aws_subnet.my_subnet.id
 
   user_data = <<-EOF
               #!/bin/bash
