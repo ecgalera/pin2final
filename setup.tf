39784e97e6c4d1c1d4dbc40ba1d100cc16ade79b
Eduardo Galera <82102759+ecgalera@users.noreply.github.com>
2024-11-07 17:03:42 -0300

    Update setup.tf

--- setup.tf
+++ setup.tf
@@ -5,6 +5,17 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "MySubnet"
+  }
+}
+
 resource "aws_security_group" "my_sg" {
   vpc_id = aws_vpc.my_vpc.id
 
